c3792f04c44026553b63a518de8a558ac139cfce
hatuhay <alberto@siles.pe>
2013-05-27 13:28:52 -0500

    Issue #0005 by @hatuhay: Syntax corrections.

--- template.php
+++ template.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * @file
+*/
+
+/**
  * Override or insert variables into the page template for HTML output.
  * For taxonomy page, insert vocabulary id class.
  * Define the variable to activate responsive behaivor.
@@ -12,7 +16,7 @@ function bootstrap_barrio_preprocess_html(&$variables) {
   }
   if (theme_get_setting('toggle_responsive')) {
     $variables['mobile_friendly'] = TRUE;
-  } 
+  }
   else {
     $variables['mobile_friendly'] = FALSE;
   }
@@ -20,6 +24,7 @@ function bootstrap_barrio_preprocess_html(&$variables) {
 
 /**
  * Override or insert variables into the page template for page output.
+ *
  * Sets the widths of the main columns of the page.
  */
 function bootstrap_barrio_preprocess_page(&$variables) {
@@ -29,7 +34,7 @@ function bootstrap_barrio_preprocess_page(&$variables) {
   $variables['nav_style'] = _bootstrap_barrio_nav_style(theme_get_setting('nav_style'));
   if (!theme_get_setting('print_content') && drupal_is_front_page()) {
     $variables['print_content'] = FALSE;
-  } 
+  }
   else {
     $variables['print_content'] = TRUE;
   }
@@ -41,7 +46,7 @@ function bootstrap_barrio_preprocess_page(&$variables) {
   if (theme_get_setting('fluid') || (arg(0) == 'admin')) {
     $variables['container'] = 'container-fluid';
     $variables['row'] = 'row-fluid';
-  } 
+  }
   else {
     $variables['container'] = 'container';
     $variables['row'] = 'row';
@@ -50,6 +55,7 @@ function bootstrap_barrio_preprocess_page(&$variables) {
 
 /**
  * Returns with of content region.
+ *
  * Calculates content width based on first and second column width parameters.
  */
 function _bootstrap_barrio_content_width() {
@@ -61,7 +67,9 @@ function _bootstrap_barrio_content_width() {
 
 /**
  * Returns a list of blocks.
- * Uses Drupal block interface and appends any blocks assigned by the Context module.
+ *
+ * Uses Drupal block interface and appends any blocks 
+ * assigned by the Context module.
  * Taken from Fusion Core.
  */
 function _bootstrap_barrio_block_list($region) {
@@ -83,13 +91,16 @@ function _bootstrap_barrio_nav_style($theme_nav_style) {
   switch ($theme_nav_style) {
     case 0:
       $nav_style = 'navbar navbar-static-top';
-      break; 
+      break;
+
     case 1:
       $nav_style = 'navbar navbar-fixed-top';
-      break; 
+      break;
+
     case 2:
       $nav_style = 'nav-collapse collapse';
-      break; 
+      break;
+
     default:
       $nav_style = 'navbar navbar-static-top';
       break;
