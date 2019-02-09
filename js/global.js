a43c27dbe3e0dd7c796c946eb7ca35952d0b6760
hatuhay <alberto@siles.pe>
2019-02-09 14:36:52 -0500

    Add toasts message style

--- js/global.js
+++ js/global.js
@@ -10,20 +10,28 @@
   Drupal.behaviors.bootstrap_barrio = {
     attach: function (context, settings) {
 
-	$(window).scroll(function() {
+      var position = $(window).scrollTop();
+	    $(window).scroll(function() {
         if ($(this).scrollTop() > 50){  
           $('body').addClass("scrolled");
         }
         else{
           $('body').removeClass("scrolled");
         }
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+          $('body').addClass("scrolldown");
+          $('body').removeClass("scrollup");
+        } else {
+          $('body').addClass("scrollup");
+          $('body').removeClass("scrolldown");
+        }
+        position = scroll;
       });
 
       var toggleAffix = function(affixElement, scrollElement, wrapper) {
-  
         var height = affixElement.outerHeight(),
             top = wrapper.offset().top;
-    
         if (scrollElement.scrollTop() >= top){
             wrapper.height(height);
             affixElement.addClass("affix");
@@ -32,23 +40,20 @@
             affixElement.removeClass("affix");
             wrapper.height('auto');
         }
-      
       };
-  
-
       $('[data-toggle="affix"]').each(function() {
         var ele = $(this),
-            wrapper = $('<div></div>');
-    
+          wrapper = $('<div></div>');
         ele.before(wrapper);
         $(window).on('scroll resize', function() {
-            toggleAffix(ele, $(this), wrapper);
+          toggleAffix(ele, $(this), wrapper);
         });
-    
         // init
         toggleAffix(ele, $(window), wrapper);
       });
 
+      $('.toast').toast('show');
+      
     }
   };
 
