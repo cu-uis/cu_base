70a73755b04ab4c7a71beb07883eff66dba85795
hatuhay <hatuhay@827704.no-reply.drupal.org>
2020-08-15 15:09:03 -0500

    Fix typo on navigation library

--- js/global.js
+++ js/global.js
@@ -29,6 +29,12 @@
         position = scroll;
       });
 
+      $('.dropdown-item a.dropdown-toggle').on("click", function(e){
+        $(this).next('ul').toggle();
+        e.stopPropagation();
+        e.preventDefault();
+      });
+      
     }
   };
 
