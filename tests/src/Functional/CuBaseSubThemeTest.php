11a50a6e8796eb41c370345f0af10d16702ebc46
Matt Eschenbaum <matt.eschenbaum@cu.edu>
2022-05-17 12:11:36 -0600

    Init 1.1

---
+++ tests/src/Functional/CuBaseSubThemeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\Tests\cu_base\Functional;
+
+
+/**
+ * Tests the CU Base theme.
+ *
+ * @group claro
+ */
+class CuBaseSubThemeTest extends CuBaseTest {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'cu_base_subtheme';
+
+}
---
+++ tests/src/Functional/CuBaseTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\Tests\cu_base\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the CU Base theme.
+ *
+ * @group claro
+ */
+class CuBaseTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * Install the shortcut module so that bootstrap_bario.settings has its schema
+   * checked. There's currently no way for CU Base to provide a default
+   * and have valid configuration as themes cannot react to a module install.
+   *
+   * @var string[]
+   */
+  protected static $modules = ['shortcut'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'cu_base';
+
+
+  /**
+   * Test CU Base's configuration schema.
+   */
+  public function testConfigSchema() {
+    $this->drupalLogin($this->rootUser);
+    $this->drupalGet('admin/appearance/settings/' . $this->defaultTheme);
+    $this->submitForm([], 'Save configuration');
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
